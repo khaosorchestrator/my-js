c3f31010cf383cb7e59f723f4c4b93716e992489
Lucas Almeida <lucasdelson24@gmail.com>
2020-10-24 22:46:22 +0100

    Update largestOfFour.js

--- largestOfFour.js
+++ largestOfFour.js
@@ -1,2 +1,2 @@
 // This function return the largest number of the four nest array
-let largestOfFour = arr => arr.map(arr => arr.reduce((prev, cur) => (prev > cur) ? prev: cur));
+let largestOfFour = arr => arr.map(arr => arr.reduce((prev, cur) => (prev > cur) ? prev : cur))
