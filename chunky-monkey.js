f3c003f320bf750c6621206bfa9dadd99683a7c6
Lucas Almeida <developer.lucas24@gmail.com>
2021-03-11 01:13:57 +0100

    refactor!: drop unusual lines

--- chunky-monkey.js
+++ chunky-monkey.js
@@ -1,15 +1,12 @@
 function chunkArrayInGroups(arr, size) {
-  let array = []
+  let array = [];
 
   for (let i = 0; i < arr.length + 1; i++) {
-    while(i) {
-      array.push(arr.splice(0, size))
-      i--
-    } 
+    while (i) {
+      array.push(arr.splice(0, size));
+      i--;
+    }
   }
 
-  return array
+  return array;
 }
-
-let arr = chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
-console.log(arr)
