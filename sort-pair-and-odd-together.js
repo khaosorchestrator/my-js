76f3c979067b9ccfe080235f1ac120cc0d46f0ea
Lucas Almeida <lucasdelson24@gmail.com>
2021-04-27 01:33:07 +0100

    Update and rename sortPairAndOddTogether.js to sort-pair-and-odd-together.js

--- sort-pair-and-odd-together.js
+++ sort-pair-and-odd-together.js
@@ -1,8 +1,6 @@
-const arr = [11, 15, 3, 13, 10, 4, 32, 34, 543, 3456, 654, 567, 87, 6789, 98]
-
 function sortPairAndOddTogether(arr) {
   let arr1 = arr.filter((element) => element % 2 == 0).sort((a, b) => a - b) // Filter the pairs and sort in ascending order. 
   let arr2 = arr.filter((element) => element % 2 != 0).sort((a, b) => b - a) // Filter the odd and sort in descending order.
   
-  return [...arr1, ...arr2].map((element) => console.log(element))
+  return [...arr1, ...arr2]
 }
