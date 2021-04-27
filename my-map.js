0bea7b6e37137c4c0d04940e5bcb05e764b778be
Lucas Almeida <lucasdelson24@gmail.com>
2021-04-27 01:29:16 +0100

    Update my-map.js

--- my-map.js
+++ my-map.js
@@ -2,7 +2,7 @@ let array = [23, 65, 98, 5]
 
 Array.prototype.myMap = function (callback) {
 	let newArray = []
-	this.forEach (el => newArray.push(callback(el)))
+	this.forEach (element => newArray.push(callback(element)))
 	
 	return newArray
 }
