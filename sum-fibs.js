f4a5d17d8034c06f04f70f473b709d799f221910
Lucas Almeida <lucasdelson24@gmail.com>
2021-05-02 04:45:02 +0100

    Create sum-fibs.js

---
+++ sum-fibs.js
@@ -0,0 +1,10 @@
+let sumFibs = n => {
+  let arr = [0, 1]
+
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1])
+  }
+  return arr.filter(num => num % 2 && num <= n).reduce((acm, acv) => acm + acv)
+}
+
+console.log(sumFibs(10))
