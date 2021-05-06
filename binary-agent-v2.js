604af8ad49bbb4e79eb3ca5fb3bc778c08a03986
Lucas Almeida <lucasdelson24@gmail.com>
2021-05-06 14:20:19 +0100

    Update binary-agent-v2.js

--- binary-agent-v2.js
+++ binary-agent-v2.js
@@ -1,2 +1,1 @@
-let binaryAgent = str =>
-  String.fromCharCode(...str.split(" ").map(char => parseInt(char, 2)))
+let binaryAgent = str => String.fromCharCode(...str.split(" ").map(char => parseInt(char, 2)))
