9fcfeac0220510a76d32991adbce82d95687ccb2
Lucas Almeida <lucasdelson24@gmail.com>
2021-05-14 21:25:33 +0100

    Create truth-check-v2.js

---
+++ truth-check-v2.js
@@ -0,0 +1,4 @@
+let truthCheck = (collection, pre) => collection.every(obj => obj.hasOwnProperty(pre) && Boolean(obj[pre]))
+
+let value = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")
+console.log(value)
